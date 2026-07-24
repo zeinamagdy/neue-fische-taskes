f5edd69cf72fb4b1b7f6605c17c524e201657bef
Zeinab Magdy <zainabmagdy8@gmail.com>
2026-07-24 14:47:19 +0200

    mplement a search by title

--- recap-project-1/src/index.ts
+++ recap-project-1/src/index.ts
@@ -1,59 +1,23 @@
-import { fetchBooks } from "./services/service";
+import { fetchBooks, searcbyTitle } from "./services/service";
 import type { BookPreview } from "./types/types";
-console.log("bunw");
+import { createRow } from "./helper/helper";
 
 const books: BookPreview[] = await fetchBooks();
 console.log("fetch books", books[0]);
 
 const tbody =
   document.querySelector<HTMLTableSectionElement>("#books-table tbody");
-
+const search = document.querySelector<HTMLInputElement>("#search");
+// display books in index
 for (const book of books) {
-  console.log("books", book);
   tbody?.appendChild(createRow(book));
 }
-
-function createRow(book: BookPreview): HTMLTableRowElement {
-  const row = document.createElement("tr");
-
-  const favICon = document.createElement("button");
-  favICon.className = "button button-clear fav-btn"
-  favICon.innerHTML = `<svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      class="fav"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                      />
-                    </svg>`
-
-  const titleCell = document.createElement("td");
-  titleCell.textContent = book.title;
-
-  const isbnCell = document.createElement("td");
-  isbnCell.textContent = book.isbn;
-
-  const authorCell = document.createElement("td");
-  authorCell.textContent = book.author;
-
-  const publisherCell = document.createElement("td");
-  publisherCell.textContent = book.publisher;
-
-  const detailsBtn = document.createElement("button");
-  detailsBtn.textContent = "Detail";
-
-  row.appendChild(favICon);
-  row.appendChild(titleCell);
-  row.appendChild(isbnCell);
-  row.appendChild(authorCell);
-  row.appendChild(publisherCell);
-  row.appendChild(detailsBtn);
-
-  return row;
-}
+// display books by search (title)
+search?.addEventListener("input", async (e) => {
+  const target = e.target as HTMLInputElement;
+  tbody?.replaceChildren();
+  const filteredBooks: BookPreview[] = await searcbyTitle(target.value);
+  for (const book of filteredBooks) {
+    tbody?.appendChild(createRow(book));
+  }
+});
--- recap-project-1/src/services/service.ts
+++ recap-project-1/src/services/service.ts
@@ -1,11 +1,18 @@
-import type { BookPreview} from "../types/types";
+import type { BookPreview } from "../types/types";
 
 const base_url = "http://localhost:4730";
 
 export async function fetchBooks(): Promise<BookPreview[]> {
   const response = await fetch(base_url + "/books");
   const data = await response.json();
-    console.log("book", data[0] as BookPreview[])// it deosnt work
+  console.log("book", data[0] as BookPreview[]); // it deosnt work in console
+  return data as BookPreview[];
+}
 
+export async function searcbyTitle(title: string): Promise<BookPreview[]> {
+    console.log("searcg", title)
+  const response = await fetch(`${base_url}/books?q=${title}`);
+  const data = await response.json();
+  console.log("book search", data[0] as BookPreview[]); // it deosnt work in console
   return data as BookPreview[];
 }
